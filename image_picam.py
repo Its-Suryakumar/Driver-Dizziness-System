001318cc635fed44e641c12b215bfbe12202a416
Sainath <sainathganesh1@gmail.com>
2019-04-01 00:24:10 +0530

    indentation error fixes

--- image_picam.py
+++ image_picam.py
@@ -12,7 +12,7 @@ import RPi.GPIO as GPIO
 import Adafruit_CharLCD as LCD
 
 GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM) 
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(12, GPIO.OUT)
 p = GPIO.PWM(12, 1000)
 p.start(50)
@@ -32,15 +32,14 @@ lcd_rows = 2
 # Initialize the LCD using the pins above.
 lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
 							lcd_columns, lcd_rows, lcd_backlight)
-
-    
 t = 0.01
+
 def eye_aspect_ratio(eye):
-    A = distance.euclidean(eye[1], eye[5])
-    B = distance.euclidean(eye[2], eye[4])
-    C = distance.euclidean(eye[0], eye[3])
-    ear = (A + B) / (2.0 * C)
-    return ear
+	A = distance.euclidean(eye[1], eye[5])
+	B = distance.euclidean(eye[2], eye[4])
+	C = distance.euclidean(eye[0], eye[3])
+	ear = (A + B) / (2.0 * C)
+	return ear
 
 thresh = 0.25
 frame_check = 5.0
@@ -52,17 +51,19 @@ predict = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
 cam = PiCamera()
 
-
 flag = 0.0
 reset = False
+
 while True:
-    imgResp = PiRGBArray(cam)
-    cam.capture(imgResp, format="bgr")
-    frame = imgResp.array
-    frame = imutils.resize(frame, width=450)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    subjects = detect(gray, 0)
-    for subject in subjects:
+
+	imgResp = PiRGBArray(cam)
+	cam.capture(imgResp, format="bgr")
+	frame = imgResp.array
+	frame = imutils.resize(frame, width=450)
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	subjects = detect(gray, 0)
+
+	for subject in subjects:
 		shape = predict(gray, subject)
 		shape = face_utils.shape_to_np(shape)#converting to NumPy Array
 		leftEye = shape[lStart:lEnd]
@@ -74,13 +75,14 @@ while True:
 		rightEyeHull = cv2.convexHull(rightEye)
 		cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
 		cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+
 		if ear < thresh:
 			flag += 1.0
 			print (flag)
 			if flag >= frame_check/2 - 1:
 				lcd.clear()
 				lcd.message('Warning! \n Threshold: ' + str(flag/frame_check))
-			if flag >= frame_check:
+			elif flag >= frame_check:
 				lcd.clear()
 				lcd.message('Alert!!! SOS')
 				cv2.putText(frame, "****************ALERT!****************", (10, 30),
@@ -88,17 +90,17 @@ while True:
 				cv2.putText(frame, "****************ALERT!****************", (10,325),
 						cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 				if not reset:
-						reset = True
-						call.call("SOS!!! You've been registered as the emergency contact for <Person>")
+					reset = True
+					call.call("SOS!!! You've been registered as the emergency contact for <Person>")
 			else:
 				lcd.clear()
 				lcd.message('Detecting... \n Threshold: ' + str(flag/frame_check))
 		else:
 			flag = 0.0
-    
+
 	cv2.imshow("Frame", frame)
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord("x"):
-            cv2.destroyAllWindows()
-            cap.release()
-            break
+	key = cv2.waitKey(1) & 0xFF
+	if key == ord("x"):
+			cv2.destroyAllWindows()
+			cap.release()
+			break
